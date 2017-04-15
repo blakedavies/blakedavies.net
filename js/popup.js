9e51f70dcc43da847fe3484c3fd96a5d25f532a8
Blake Davies <blakedavies1992@gmail.com>
2017-04-15 15:19:41 +1000

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){setTimeout(function(){$('div.popup').removeClass("hidden");},3000);$("div.popup div.box a").click(function(){$('div.popup').addClass("hidden");});});
+$(window).load(function(){setTimeout(function(){$('div.popup').removeClass("hidden");},3000);$("div.popup div.box a").click(function(){$('div.popup').addClass("hidden");});});
