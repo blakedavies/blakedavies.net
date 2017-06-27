3bbb63ad5059cbf20dd40da8af7da9fc5b5f00a0
Blake Davies <blakedavies1992@gmail.com>
2017-06-28 08:58:20 +1000

    Update stickyheader.js

--- js/stickyheader.js
+++ js/stickyheader.js
@@ -1,1 +1,1 @@
-$(window).scroll(function(){if($(window).scrollTop()>=$('#home div.layer div.box').position().top-74){$("#home div.layer header").addClass("sticky");}else{$("#home div.layer header").removeClass("sticky");}});
+$(window).scroll(function(){if($(window).scrollTop()>=$('#home div.parallax-window div.box').position().top-74){$("#home div.parallax-window header").addClass("sticky");}else{$("#home div.parallax-window header").removeClass("sticky");}});
