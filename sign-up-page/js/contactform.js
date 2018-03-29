5ad5ff282d179ce4f4da41cafb44c1b2900510a8
Blake Davies <blakedavies1992@gmail.com>
2018-03-30 06:45:31 +1000

    Update contactform.js

--- sign-up-page/js/contactform.js
+++ sign-up-page/js/contactform.js
@@ -1,22 +1,23 @@
 $('#gform').submit(function(e) {
-	      e.preventDefault();
-	      $.ajax({
-	        url: "https://docs.google.com/a/blakedavies.net/forms/d/e/1FAIpQLSc8GOOnB41kD7RnRh1ME7yYlB-dGX1ZU11vBK1YMoONujNuQw/formResponse",
-	        data: $(this).serialize(),
-	        type: "POST",
-	        dataType: "xml",
-	        success: function(data) {
-	          console.log('Submission successful');
-	        },
-	        error: function(xhr, status, error) {
-	          console.log('Submission failed: ' + error);
-	        }
-	      });
-	    });
-	    $('#gform').on('submit', function(e) {
-	  		$('#gform *').fadeOut(0);
-	  		$('#contact-box *').fadeOut(0);
-	  		$('#gform').prepend(
-	    		'<div class="thank-you-box"><div class="inner"><h3 class="thank-you">Thank-you!!<h3><p class="thank-you">Your completed form has been submitted successfully.<br/>You will be sent your pre-filled Application Form by email to sign.</p></div></div>'
-	    	);
-	  	});
+	e.preventDefault();
+	$.ajax({
+		url: "https://docs.google.com/a/blakedavies.net/forms/d/e/1FAIpQLSc8GOOnB41kD7RnRh1ME7yYlB-dGX1ZU11vBK1YMoONujNuQw/formResponse",
+		data: $(this).serialize(),
+		type: "POST",
+		dataType: "xml",
+		success: function(data) {
+			console.log('Submission successful');
+		},
+		error: function(xhr, status, error) {
+			console.log('Submission failed: ' + error);
+		}
+	});
+});
+
+$('#gform').on('submit', function(e) {
+	$('#gform *').fadeOut(0);
+	$('#contact-box *').fadeOut(0);
+	$('#gform').prepend(
+		'<div class="thank-you-box"><div><p class="thank-you">Thank-you!!</p><p class="thank-you">Your completed form has been submitted successfully.</p><p class="thank-you">You will be sent your pre-filled Application Form by email to sign.</p></div></div>'
+	);
+});
